08e274055d53eef0896c6e40b3d549d1fec0aed4
SnhsDs <snehashis187@gmail.com>
2025-12-03 13:20:36 +0530

    tree questions

--- Leetcode/Q493.java
+++ Leetcode/Q493.java
@@ -1,5 +1,3 @@
-package Leetcode;
-
 import java.util.Arrays;
 
 public class Q493 {
@@ -21,8 +19,53 @@ public class Q493 {
         System.out.println();
     }
   }
-  public int reversePairs(int[] arr) {
-    
+  static int result = 0;
+    public int reversePairs(int[] nums) {
+        long[] arr = new long[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+        result = 0;
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+  public static int mergeSort(long[] arr, int low, int high) {
+    if (low < high) {
+      int mid = low + (high - low) / 2;
+      mergeSort(arr, low, mid);
+      mergeSort(arr, mid + 1, high);
+      merge(arr, low, mid, high);
+    }
+    return result;
+  }
+  public static int merge(long[] arr, int low, int mid, int high) {
+    long[] a = new long[high - low + 1];
+    int i = low, j = mid + 1, k = 0;
+        while (i <=mid && j <=high) {
+            if (arr[i] > (2 * arr[j])) {
+                result += (mid - i + 1);
+                j++;
+            } else  i++;
+        }
+        i = low;
+        j = mid + 1;
+    while (i <=mid && j <=high) {
+      if (arr[i] <= arr[j]) {
+        a[k++] = arr[i++];
+      }
+            else {
+        a[k++] = arr[j++];
+      }
+    }
+    while (i <= mid) {
+      a[k++] = arr[i++];
+    }
+    while (j <= high) {
+      a[k++] = arr[j++];
+    }
+    for (int c = 0; c < k; c++) {
+      arr[low + c] = a[c];
+    }
+    return result;
   }
 
 }
